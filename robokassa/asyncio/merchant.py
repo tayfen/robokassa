769683323438b7cdfefcdd370f82ca9a1f8ee9d1
Ben Puls <mail@benpuls.ru>
2024-10-20 02:07:38 +0500

    add: merchant methods

---
+++ robokassa/asyncio/merchant.py
@@ -0,0 +1,25 @@
+from robokassa.asyncio.connection import Requests
+from robokassa.utils import HttpResponseValidator
+
+
+class AsyncMerchant:
+    def __init__(self, http: Requests, merchant_login: str) -> None:
+        self._http = http.connection
+
+        self._merchant_login = merchant_login
+        print(merchant_login)
+
+    async def get_currencies(self, language: str) -> dict:
+        async with self._http as conn:
+            response = await conn.post(
+                url="WebService/Service.asmx/GetCurrencies",
+                data={
+                    "MerchantLogin": self._merchant_login,
+                    "Language": language,
+                },
+            )
+            print(response.url)
+            validated_response = HttpResponseValidator(
+                response, False
+            ).validate_http_response()
+            return validated_response
