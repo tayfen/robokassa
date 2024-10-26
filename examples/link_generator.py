cbd3503b7ab68743a6aa223e25f4329fb8ce596d
Ben Puls <mail@benpuls.ru>
2024-10-26 05:29:27 +0500

    add: link generator example

--- examples/link_generator.py
+++ examples/link_generator.py
@@ -5,10 +5,7 @@ robokassa = Robokassa(
     password1="password",
     password2="password",
     is_test=False,
-    algorithm=HashAlgorithm.md5
+    algorithm=HashAlgorithm.md5,
 )
 
-my_link = robokassa.payment.link.generate_by_script(
-    out_sum=1000,
-    inv_id=0
-)
+my_link = robokassa.create_link_to_payment_page_by_script(out_sum=1000, inv_id=0)
