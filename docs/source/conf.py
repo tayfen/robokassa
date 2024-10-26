0a1dcac6bfda71dbf8390a5b05d7ad6812e94527
Ben Puls <mail@benpuls.ru>
2024-10-26 05:30:18 +0500

    reformat: quotes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,24 +6,24 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'robokassa'
-copyright = '2024, Ben Puls'
-author = 'Ben Puls'
+project = "Robokassa"
+copyright = "2024, Ben Puls"
+author = "Ben Puls"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
+# html_logo = '_static/robokassa_logo.png'
 
 highlight_language = "python3"
